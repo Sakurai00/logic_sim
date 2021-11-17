d2b6e3b2f6136d54b4b909664a52c6626116e26a
Sakurai <K.Sakurai_2000@outlook.com>
2021-11-17 20:36:24 +0900

    fix: define struct, nodo_list

--- main.h
+++ main.h
@@ -3,11 +3,11 @@
 
 #include <stdbool.h>
 
-#include "read_logic.h"
+// #include "read_logic.h"
 
-enum logic { wire, and, or, not, xor, nor, nand };
+enum logic { WIRE, AND, OR, NOT, XOR, NOR, NAND };
 
-typedef struct {
+struct logic_gate {
     char name[128];
     enum logic type;
     int value;
@@ -15,10 +15,12 @@ typedef struct {
     bool is_output;
     int in_num;
     struct logic_gate *in_gate[10];
-    struct logic_gate *out_gate[10];
-} logic_gate;
+    struct logic_gate *out_gate[32];
+};
 
-extern logic_gate node_list[128];
+typedef struct logic_gate logic_gate;
+
+extern logic_gate *node_list[128];
 extern int gate_num;
 
 #endif
